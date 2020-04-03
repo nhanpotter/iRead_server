f9a70900bc975cf5dbb482d695a34ad936ad6698
nhanpotter <nhanpotter8a@gmail.com>
2020-04-03 12:19:19 +0800

    Enable signals.py

--- book/signals.py
+++ book/signals.py
@@ -5,18 +5,18 @@ from django.contrib.auth.models import User
 from .models import Rating
 from .tasks import train_model_new_user, train_model_new_rating
 
-# @receiver(post_save, sender=User)
-# def train_model_when_new_user(sender, **kwargs):
-#     user = kwargs['instance']
-#     created = kwargs['created']
-#     if not created or user.is_staff:
-#         return
-#     train_model_new_user.delay()
+@receiver(post_save, sender=User)
+def train_model_when_new_user(sender, **kwargs):
+    user = kwargs['instance']
+    created = kwargs['created']
+    if not created or user.is_staff:
+        return
+    train_model_new_user.delay()
 
-# @receiver(post_save, sender=Rating)
-# def train_model_when_new_rating(sender, **kwargs):
-#     train_model_new_rating.delay()
+@receiver(post_save, sender=Rating)
+def train_model_when_new_rating(sender, **kwargs):
+    train_model_new_rating.delay()
 
-# @receiver(post_delete, sender=User)
-# def train_model_when_delete_user(sender, **kwargs):
-#     train_model_new_user.delay()
+@receiver(post_delete, sender=User)
+def train_model_when_delete_user(sender, **kwargs):
+    train_model_new_user.delay()
